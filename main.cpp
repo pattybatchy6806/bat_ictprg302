92f3cab32c521d8715a3bbf315d0cdbc82d3f74f
pattybatchy6806 <139300792+pattybatchy6806@users.noreply.github.com>
2023-07-26 12:49:15 +1000

    created project
    added SFML -2.6.0 and setup a window

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Student on 12/7/2023.
+//
+
+#include <iostream>
+
+#include <SFML/Window.hpp>
+
+int main()
+{
+    sf::Window window;
+
+   window.create(sf::VideoMode(800, 600), "My window");
+
+    sf::Event event;
+
+   while (window.isOpen())
+   {
+       if(event.type == sf::Event::Closed)
+           window.close();
+   }
+
+
+
+    return 0;
+}
